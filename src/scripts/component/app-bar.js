c25c31b0007299d05e5c33e4eead7803b667d39c
MushlihNur <nurismush@gmail.com>
2022-05-29 00:44:51 +0700

    feat: simplify navbar & add Logo.png

--- src/scripts/component/app-bar.js
+++ src/scripts/component/app-bar.js
@@ -5,27 +5,27 @@ class AppBar extends HTMLElement {
 
     _render() {
         this.innerHTML = `
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <style>
+          .bg-navbar {
+            background-color: #00C2FF;
+            border-radius: 0px 0px 20px 20px;
+          }
+        </style>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-navbar p-3">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand fs-3" href="#">
+                Yok Jalan
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled">Disabled</a>
-                </li>
-              </ul>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                <a class="nav-link active" href="#">Jelajah</a>
+                <a class="nav-link active" href="#">Favorit</a>
+                <a class="nav-link active" href="#">Tentang Kami</a>
+              </div>
             </div>
           </div>
         </nav>
