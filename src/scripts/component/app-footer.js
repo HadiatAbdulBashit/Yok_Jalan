f0cc366b30ea9113e7077b424c25ce7f1f1ccd12
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2022-05-28 20:40:20 +0700

    membenarkan bug eslint

--- src/scripts/component/app-footer.js
+++ src/scripts/component/app-footer.js
@@ -1,15 +1,15 @@
 class AppFooter extends HTMLElement {
-    connectedCallback() {
-      this._render();
-    }
-  
-    _render() {
-      this.innerHTML = `
+  connectedCallback() {
+    this._render();
+  }
+
+  _render() {
+    this.innerHTML = `
         <footer>
           <p>Copyright © 2022 - Yok Jalan</p>
         </footer>
       `;
-    }
+  }
 }
-  
+
 customElements.define('app-footer', AppFooter);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
 
 console.log('Yok Jalan!');
