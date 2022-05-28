59837c7db3d819b40bef837499802b512c1de9b7
MushlihNur <nurismush@gmail.com>
2022-05-28 20:18:01 +0700

    feat: menambahkan komponen navbar & footer

---
+++ src/scripts/component/app-footer.js
@@ -0,0 +1,15 @@
+class AppFooter extends HTMLElement {
+    connectedCallback() {
+      this._render();
+    }
+  
+    _render() {
+      this.innerHTML = `
+        <footer>
+          <p>Copyright © 2022 - Yok Jalan</p>
+        </footer>
+      `;
+    }
+}
+  
+customElements.define('app-footer', AppFooter);
