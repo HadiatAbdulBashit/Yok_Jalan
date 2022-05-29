a184e777fa624dcfc56f7d77536a077c71299c4e
MushlihNur <nurismush@gmail.com>
2022-05-29 20:00:10 +0700

    feat: add style app-bar

--- src/scripts/component/app-bar.js
+++ src/scripts/component/app-bar.js
@@ -6,25 +6,42 @@ class AppBar extends HTMLElement {
     _render() {
         this.innerHTML = `
         <style>
-          .bg-navbar {
+          .bg__navbar {
             background-color: #00C2FF;
             border-radius: 0px 0px 20px 20px;
           }
+
+          .nav__title,
+          .nav__item {
+            display: inline-block;
+            text-decoration: none;
+            color: #FFFFFF;
+            min-width : 44px;
+            min-height: 44px;
+            border-radius: 40px;
+          }
+
+          .nav__item:hover {
+            text-decoration: underline;
+            color: #00C2FF;
+            background-color: #FFFFFF;
+          }
         </style>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-navbar p-3">
+        
+        <nav class="navbar navbar-expand-lg bg__navbar p-3 sticky-top">
           <div class="container-fluid">
-            <a class="navbar-brand fs-3" href="#">
-                Yok Jalan
+            <a class="navbar-brand fs-3 nav__title" href="#">
+              Yok Jalan
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Beranda</a>
-                <a class="nav-link active" href="#">Jelajah</a>
-                <a class="nav-link active" href="#">Favorit</a>
-                <a class="nav-link active" href="#">Tentang Kami</a>
+                <a class="nav-link active nav__item" aria-current="page" href="#/Home">Beranda</a>
+                <a class="nav-link active nav__item" href="#/Explore">Jelajah</a>
+                <a class="nav-link active nav__item" href="#/Favorite">Favorit</a>
+                <a class="nav-link active nav__item" href="#/About">Tentang Kami</a>
               </div>
             </div>
           </div>
